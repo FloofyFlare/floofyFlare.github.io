9cc30bae4da63c58f3518de1ed3020f6b91365be
Raymond Flowers <flogamesapps@gmail.com>
2023-05-19 15:46:30 -0400

    hi

---
+++ chunks/app/_nuxt/index-styles.a055e09e.mjs
@@ -0,0 +1,6 @@
+const index_vue_vue_type_style_index_0_scoped_18d87abe_lang = "ul[data-v-18d87abe]{list-style:disc}li[data-v-18d87abe]{padding:5px}h2[data-v-18d87abe]{font-size:x-large}h3[data-v-18d87abe]{font-size:large}a[data-v-18d87abe]{text-decoration:underline}nav[data-v-18d87abe]{position:fixed}";
+
+const indexStyles_a055e09e = [index_vue_vue_type_style_index_0_scoped_18d87abe_lang];
+
+export { indexStyles_a055e09e as default };
+//# sourceMappingURL=index-styles.a055e09e.mjs.map
---
+++ chunks/app/client.manifest.mjs
@@ -0,0 +1,207 @@
+const client_manifest = {
+  "_TopHeader.3a9f6e5a.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "TopHeader.3a9f6e5a.js",
+    "imports": [
+      "_nuxt-link.31e55ae4.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_nuxt-link.31e55ae4.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "nuxt-link.31e55ae4.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "images/20230513_214423.jpg": {
+    "resourceType": "image",
+    "mimeType": "image/jpeg",
+    "file": "20230513_214423.da688541.jpg",
+    "src": "images/20230513_214423.jpg"
+  },
+  "images/cardanoCode.png": {
+    "resourceType": "image",
+    "mimeType": "image/png",
+    "file": "cardanoCode.4aeffd48.png",
+    "src": "images/cardanoCode.png"
+  },
+  "images/national-career-readiness-certificate-platinum.png": {
+    "resourceType": "image",
+    "mimeType": "image/png",
+    "file": "national-career-readiness-certificate-platinum.090ffa54.png",
+    "src": "images/national-career-readiness-certificate-platinum.png"
+  },
+  "images/snowQueen.png": {
+    "resourceType": "image",
+    "mimeType": "image/png",
+    "file": "snowQueen.5959918a.png",
+    "src": "images/snowQueen.png"
+  },
+  "images/webpage.png": {
+    "resourceType": "image",
+    "mimeType": "image/png",
+    "file": "webpage.d31b71e0.png",
+    "src": "images/webpage.png"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
+    "resourceType": "style",
+    "file": "error-404.8bdbaeb8.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "error-404.5817a651.js",
+    "imports": [
+      "_nuxt-link.31e55ae4.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue"
+  },
+  "error-404.8bdbaeb8.css": {
+    "file": "error-404.8bdbaeb8.css",
+    "resourceType": "style"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
+    "resourceType": "style",
+    "file": "error-500.b63a96f5.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "error-500.1a1a929b.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+  },
+  "error-500.b63a96f5.css": {
+    "file": "error-500.b63a96f5.css",
+    "resourceType": "style"
+  },
+  "node_modules/nuxt/dist/app/entry.css": {
+    "resourceType": "style",
+    "file": "entry.ed33d4d6.css",
+    "src": "node_modules/nuxt/dist/app/entry.css"
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "css": [
+      "entry.ed33d4d6.css"
+    ],
+    "dynamicImports": [
+      "virtual:nuxt:/home/floofyflare/code/VueProjects/portfolio/.nuxt/error-component.mjs"
+    ],
+    "file": "entry.b0b62e5d.js",
+    "isEntry": true,
+    "src": "node_modules/nuxt/dist/app/entry.js"
+  },
+  "entry.ed33d4d6.css": {
+    "file": "entry.ed33d4d6.css",
+    "resourceType": "style"
+  },
+  "pages/index.css": {
+    "resourceType": "style",
+    "file": "index.be5f264a.css",
+    "src": "pages/index.css"
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "assets": [
+      "20230513_214423.da688541.jpg",
+      "national-career-readiness-certificate-platinum.090ffa54.png"
+    ],
+    "css": [],
+    "file": "index.95a9f183.js",
+    "imports": [
+      "_TopHeader.3a9f6e5a.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_nuxt-link.31e55ae4.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/index.vue"
+  },
+  "index.be5f264a.css": {
+    "file": "index.be5f264a.css",
+    "resourceType": "style"
+  },
+  "20230513_214423.da688541.jpg": {
+    "file": "20230513_214423.da688541.jpg",
+    "resourceType": "image",
+    "mimeType": "image/jpeg"
+  },
+  "national-career-readiness-certificate-platinum.090ffa54.png": {
+    "file": "national-career-readiness-certificate-platinum.090ffa54.png",
+    "resourceType": "image",
+    "mimeType": "image/png"
+  },
+  "pages/projects.vue": {
+    "resourceType": "script",
+    "module": true,
+    "assets": [
+      "Screen_Recording_20210906-145727_Flight.2567d6f1.mp4",
+      "cardanoCode.4aeffd48.png",
+      "webpage.d31b71e0.png",
+      "snowQueen.5959918a.png"
+    ],
+    "file": "projects.53bb5f04.js",
+    "imports": [
+      "_TopHeader.3a9f6e5a.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_nuxt-link.31e55ae4.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/projects.vue"
+  },
+  "Screen_Recording_20210906-145727_Flight.2567d6f1.mp4": {
+    "file": "Screen_Recording_20210906-145727_Flight.2567d6f1.mp4",
+    "resourceType": "video"
+  },
+  "cardanoCode.4aeffd48.png": {
+    "file": "cardanoCode.4aeffd48.png",
+    "resourceType": "image",
+    "mimeType": "image/png"
+  },
+  "webpage.d31b71e0.png": {
+    "file": "webpage.d31b71e0.png",
+    "resourceType": "image",
+    "mimeType": "image/png"
+  },
+  "snowQueen.5959918a.png": {
+    "file": "snowQueen.5959918a.png",
+    "resourceType": "image",
+    "mimeType": "image/png"
+  },
+  "videos/Screen_Recording_20210906-145727_Flight.mp4": {
+    "resourceType": "video",
+    "file": "Screen_Recording_20210906-145727_Flight.2567d6f1.mp4",
+    "src": "videos/Screen_Recording_20210906-145727_Flight.mp4"
+  },
+  "virtual:nuxt:/home/floofyflare/code/VueProjects/portfolio/.nuxt/error-component.mjs": {
+    "resourceType": "script",
+    "module": true,
+    "dynamicImports": [
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "file": "error-component.df230fcc.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "virtual:nuxt:/home/floofyflare/code/VueProjects/portfolio/.nuxt/error-component.mjs"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
